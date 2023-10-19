7a0d40f4c9bafd8eaf3a0f75b3f3a5a1e1bd98e7
Moritz Theis <moritz@snappx.io>
2023-10-19 12:24:12 +0200

    implement example dbus app

--- bin/dart_dbus.dart
+++ bin/dart_dbus.dart
@@ -3,22 +3,22 @@ import 'package:dbus/dbus.dart';
 Future<void> main() async {
   final client = DBusClient.session();
   final object = DBusRemoteObject(client,
-      name: 'de.snapp.SensorService', path: DBusObjectPath('/Core/Sensor'));
+      name: 'com.example.SampleService', path: DBusObjectPath('/SomeObject'));
 
   final response = await object.callMethod(
-      'de.snapp.SensorInterface', 'GetSensorValue', [],
-      replySignature: DBusSignature('snapp.SensorValue'));
+      'com.example.SampleInterface', 'GetSensorValue', [],
+      replySignature: DBusSignature('as'));
 
   /// convert DBusArray to List
   final result = List.from(response.values[0].asStringArray());
 
   final temp = result[0];
   final hum = result[1];
-  final co2 = result[2];
+  final pressure = result[2];
 
   print('temp: $temp');
   print('hum: $hum');
-  print('co2: $co2');
+  print('pressure: $pressure');
 
   await client.close();
 }
