725128862728ed8120782c4f515290dcf9b65a65
Moritz Theis <moritz@snappx.io>
2023-10-19 12:22:46 +0200

    inital commit

---
+++ bin/dart_dbus.dart
@@ -0,0 +1,24 @@
+import 'package:dbus/dbus.dart';
+
+Future<void> main() async {
+  final client = DBusClient.session();
+  final object = DBusRemoteObject(client,
+      name: 'de.snapp.SensorService', path: DBusObjectPath('/Core/Sensor'));
+
+  final response = await object.callMethod(
+      'de.snapp.SensorInterface', 'GetSensorValue', [],
+      replySignature: DBusSignature('snapp.SensorValue'));
+
+  /// convert DBusArray to List
+  final result = List.from(response.values[0].asStringArray());
+
+  final temp = result[0];
+  final hum = result[1];
+  final co2 = result[2];
+
+  print('temp: $temp');
+  print('hum: $hum');
+  print('co2: $co2');
+
+  await client.close();
+}
